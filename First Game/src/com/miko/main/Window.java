62dc4fcee4360905ca7d8895ce6a02602f0535e8
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-06-18 21:23:57 -0500

    Back-end from First Game

--- First Game/src/com/miko/main/Window.java
+++ First Game/src/com/miko/main/Window.java
@@ -16,13 +16,18 @@ public class Window extends Canvas {
 	public Window(int width, int height, String	title, Game game) {
 		JFrame frame = new JFrame(title);
 		
+		//Recibe un argumento de dimension con anch y altura
 		frame.setPreferredSize(new Dimension(width,height));
 		frame.setMaximumSize(new Dimension(width,height));
 		frame.setMinimumSize(new Dimension(width,height));
 		
+		//Parar el thread de Java al usar el boton de cerrar de la esquina
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Usuario puede cambiar la ventana
 		frame.setResizable(false);
+		//Para que aparezca en medio de la pantalla
 		frame.setLocationRelativeTo(null);
+		//Se añade a la pantalla la clase game
 		frame.add(game);
 		frame.setVisible(true);
 		game.start();
